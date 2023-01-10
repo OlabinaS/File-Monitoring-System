9c53caf9f8880e667f734af94290d6507240f26b
CoekCx <nemanja.ja.jovicic7@gmail.com>
2023-01-10 21:20:29 +0100

    Created IPSService, FileManagerProxy and IIPSService interface

---
+++ ServiceContracts/IIPSService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace ServiceContracts
+{
+    [ServiceContract]
+    public interface IIPSService
+    {
+        [OperationContract]
+        void LogCritical();
+
+        [OperationContract]
+        void LogInformation();
+
+        [OperationContract]
+        void LogWarning();
+    }
+}
