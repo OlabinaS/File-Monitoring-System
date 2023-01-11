6a0a0b2af3dedf65fc6c8be8def4a91a0daaedb2
OgnjenPR29 <ognjen.sekulic1408@gmail.com>
2023-01-11 14:26:11 +0100

    Added FIM component with few functionalities

---
+++ FIM/FimIPS.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceContracts;
+
+namespace FIM
+{
+    public class FimIPS : ChannelFactory<IIPSService>, IIPSService, IDisposable
+    {
+        private IIPSService factory;
+
+        public FimIPS(NetTcpBinding binding,EndpointAddress address) : base(binding, address)
+        {
+            factory = this.CreateChannel();
+        }
+
+        public void LogCritical(Alarm alarm)
+        {
+            factory.LogCritical(alarm);
+        }
+
+        public void LogInformation(Alarm alarm)
+        {
+            factory.LogInformation(alarm);
+        }
+
+        public void LogWarning(Alarm alarm)
+        {
+            factory.LogWarning(alarm);
+        }
+    }
+
+}
