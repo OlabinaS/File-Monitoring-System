c6792a3beca330c3f3c019d9a6fb4275c08a980e
Stefan <stefan.olabina@gmail.com>
2023-01-30 19:20:48 +0100

    Validate digital signature and added formatter v2

---
+++ Manager/Formatter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager
+{
+	public class Formatter
+	{
+        public static string Parser(string fullName)
+        {
+            string[] splitted = new string[] { };
+
+            if (fullName.Contains("@"))//logName@domainName
+            {
+                splitted = fullName.Split('@');
+                return splitted[0];
+            }
+            else if (fullName.Contains("\\"))//domainName\logName
+            {
+                splitted = fullName.Split('\\');
+                return splitted[1];
+            }
+            else if (fullName.Contains("CN"))//CN=logName
+            {
+                int start = fullName.IndexOf("=") + 1;
+                int end = fullName.IndexOf(";");
+                string s = fullName.Substring(start, end - start);
+                return s;
+            }
+
+            return fullName;
+        }
+    }
+}
